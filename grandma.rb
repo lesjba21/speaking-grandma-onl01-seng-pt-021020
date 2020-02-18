98ee033d634f21c594d75d753d5c02424130f782
Lesly Jean-Baptiste <lesjba56@yahoo.com>
2020-02-18 01:31:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,22 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(phrase) 
+  if phrase == "I LOVE YOU GRANDMA!"
+     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase !=   
+  end
+  
+
+   
+   
+   
+   
+   
+   
+   
+   
+
+   
+   
+   
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
@@ -11,3 +29,5 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
